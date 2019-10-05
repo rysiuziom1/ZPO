5d85dd43ef499392fc037d11b5ed32e51e34c519
Jacek <rysiuziom1@gmail.com>
2019-10-05 09:22:49 +0200

    zad3 v1.1

--- LAB1/zad3/src/PESELInterpreter.java
+++ LAB1/zad3/src/PESELInterpreter.java
@@ -15,46 +15,60 @@ public class PESELInterpreter {
     }
 
     private static PersonalData getPersonalData(String pes) throws IllegalArgumentException {
+        //validate pesel regex
         if (!pes.matches("[0-9]{11}"))
-            throw new IllegalArgumentException("Wrong PESEL");
+            throw new IllegalArgumentException("Wrong PESEL - less or more then 11 digits, or some character aren't digits");
 
         int eternity = 19;
         int birthYear = Integer.parseInt(pes.substring(0, 2));
         int birthMonth = Integer.parseInt(pes.substring(2, 4));
         int birthDay = Integer.parseInt(pes.substring(4, 6));
 
+        //identify eternity
         if (birthMonth > 80) {
-            if (birthMonth > 92)
-                throw new IllegalArgumentException("Wrong PESEL");
             eternity--;
             birthMonth -= 80;
         }
-
         while (birthMonth > 20) {
             eternity++;
             birthMonth -= 20;
         }
 
+        //validate month
         if (birthMonth > 12)
-            throw new IllegalArgumentException("Wrong PESEL");
+            throw new IllegalArgumentException("Wrong PESEL - wrong month of birth");
 
+        //validate birthday
         if (birthDay > 31)
-            throw new IllegalArgumentException("Wrong PESEL");
+            throw new IllegalArgumentException("Wrong PESEL - wrong day of birth");
 
         birthYear += eternity * 100;
 
+        //validate birthday v1.1
         switch (birthMonth) {
             case 4:
             case 6:
             case 9:
             case 11:
                 if (birthDay > 30)
-                    throw new IllegalArgumentException("Wrong PESEL");
+                    throw new IllegalArgumentException("Wrong PESEL - wrong day of birth");
                 break;
             case 2:
                 if (birthDay > 29 || (birthDay > 28 && !Year.isLeap(birthYear)))
-                    throw new IllegalArgumentException("Wrong PESEL");
+                    throw new IllegalArgumentException("Wrong PESEL - wrong day of birth");
         }
+
+        //validate control number
+        char[] pesDigits = pes.toCharArray();
+        int controlNumberFromPes = Character.getNumericValue(pesDigits[pesDigits.length - 1]);
+        int controlNumber = 0;
+        int[] weighs = {9, 7, 3, 1, 9, 7, 3, 1, 9, 7};
+        for (int i = 0; i < pesDigits.length - 1; i++) {
+            controlNumber += weighs[i] * Character.getNumericValue(pesDigits[i]);
+        }
+        controlNumber %= 10;
+        if (controlNumber != controlNumberFromPes)
+            throw new IllegalArgumentException("Wrong PESEL - wrong control number");
 
         String[] months = {"styczeń", "luty", "marzec", "kwiecień", "maj", "czerwiec", "lipiec", "sierpień", "wrzesień",
                 "październik", "listopad", "grudzień"};
@@ -62,7 +76,6 @@ public class PESELInterpreter {
         String birthDate = birthDay + " " + months[birthMonth - 1] + " " + birthYear;
 
         int genderNumber = Integer.parseInt(pes.substring(9, 10));
-
         String gender = genderNumber % 2 != 0 ? "mężczyzna" : "kobieta";
 
         return new PersonalData(birthDate, gender);
