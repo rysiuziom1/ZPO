d29eb3961aa0f8f89f8503cce97aa6013c0a17e8
Jacek <rysiuziom1@gmail.com>
2019-10-05 14:42:48 +0200

    zad4

---
+++ zad4/src/Expressions.java
@@ -0,0 +1,40 @@
+import javax.swing.*;
+import java.util.HashMap;
+
+public class Expressions {
+    public static void main(String[] args) {
+        String expression = JOptionPane.showInputDialog("Please input algebraic expression");
+        if(!expression.matches("[0-9]{1,2} [+\\-*] [0-9]{1,2} [+\\-*] [0-9]{1,2}"))
+            throw new IllegalArgumentException("Wrong algebraic expression");
+        String wordExpression = algebraicToWordExpression(expression);
+        JFrame jFrame = new JFrame();
+        JOptionPane.showMessageDialog(jFrame, wordExpression);
+    }
+
+    private static String algebraicToWordExpression(String algebraicExpression) {
+        //create numbers's map
+        HashMap<String, String> numberMap = new HashMap<>();
+        String[] belowTwenty = {"zero", "jeden", "dwa", "trzy", "cztery", "pięć", "sześć", "siedem", "osiem",
+                "dziewięć", "dziesięć", "jedenaście", "dwanaście", "trzynaście", "cztenaście", "piętnaście",
+                "szesnaście", "siedemnaście", "osiemnaście", "dziwiętnaście"};
+        String[] teens = {"", "", "dwadzieścia", "trzydzieście", "czterdzieści", "pięćdziesiąt", "sześćdziesiąt",
+                "siedemdziesiąt", "osiemdziesiąt", "dziewięćdziesiąt"};
+
+        for (int i = 0; i < 100; i++) {
+            if (i < 20)
+                numberMap.put(String.valueOf(i), belowTwenty[i]);
+            else {
+                numberMap.put(String.valueOf(i), teens[i / 10] + (i % 10 == 0 ? "" : " " + belowTwenty[i % 10]));
+            }
+        }
+        numberMap.put("+", "plus");
+        numberMap.put("*", "razy");
+        numberMap.put("-", "minus");
+        String[] exprComponents = algebraicExpression.split(" ");
+        StringBuilder sb = new StringBuilder();
+        for(String s : exprComponents) {
+            sb.append(numberMap.get(s)).append(" ");
+        }
+        return sb.toString();
+    }
+}
