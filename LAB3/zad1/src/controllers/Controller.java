5a9536e2fe314c86cffea7834f5abf69cc771106
Jacek <rysiuziom1@gmail.com>
2019-10-22 15:47:21 +0200

    lab3 v3

--- LAB3/zad1/src/controllers/Controller.java
+++ LAB3/zad1/src/controllers/Controller.java
@@ -3,9 +3,7 @@ package controllers;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.Node;
 import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableRow;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 
