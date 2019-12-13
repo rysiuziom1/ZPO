aac09fb3b1176c28cde51b43aa42402d7476307e
Jacek LAPTOP <rysiuziom1@gmail.com>
2019-12-13 14:17:32 +0100

    refactor lab8 zad1

--- LAB8/zad1/src/Main.java
+++ LAB8/zad1/src/Main.java
@@ -3,11 +3,11 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        MaxSearchAlgorithms asd = new MaxSearchAlgorithms();
+        MaxSearchAlgorithms maxSearchAlgorithms = new MaxSearchAlgorithms();
         int[] data = {4, 10, 3, 7, 4, 1, 6, 2};
-        Arrays.stream(asd.getClass().getDeclaredMethods()).filter(e -> e.getName().contains("Scan")).forEach(e -> {
+        Arrays.stream(maxSearchAlgorithms.getClass().getDeclaredMethods()).filter(e -> e.getName().contains("Scan")).forEach(e -> {
             try {
-                System.out.println(e.invoke(asd, data));
+                System.out.println(e.invoke(maxSearchAlgorithms, data));
             } catch (IllegalAccessException | InvocationTargetException ex) {
                 ex.printStackTrace();
             }
